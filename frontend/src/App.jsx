1489d1eaad521fc79ac285fd38bf4d61bea5184a
Bhumi Singla <bhumi321a@gmail.com>
2025-07-12 20:42:17 +0530

    Project started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className='font-bold text-amber-700 text-4xl text-center m-20'>Hi</div>
+    </>
+  )
+}
+
+export default App
